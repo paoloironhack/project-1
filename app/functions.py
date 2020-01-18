dc8e83b024679774046614934d9956482ec6be23
Pedro Moreira <pedrodavidmoreira@gmail.com>
2020-01-18 15:56:26 +0100

    Main functionality implemented

--- app/functions.py
+++ app/functions.py
@@ -1,9 +1,10 @@
 import string
 import random
 
+
 def yesnoQuestion(q):
     a = False
-    while a not in ['Y','y','N','n']:
+    while a not in ['Y', 'y', 'N', 'n']:
         a = input(q + " [Y/N]: ")
     if a is 'y' or a is 'Y':
         return True
@@ -12,11 +13,11 @@ def yesnoQuestion(q):
     else:
         raise ValueError("Incorrect option passed. This should not happen please investigate.")
 
+
 def presentOptions():
-    options = {}
-    options['length'] = False
-    while options['length'] not in range(6,129):
-        options['length'] = int(input("How long do you want your password to be?(Min: 6 Max:128): "))
+    options = {'length': False}
+    while options['length'] not in range(15, 129):
+        options['length'] = int(input("How long do you want your password to be?(Min:15 Max:128): "))
     options['alpha'] = yesnoQuestion("Include alpha characters (a-z)?")
     options['caseSensitive'] = yesnoQuestion("Case Sensitive (A-Z,a-z)?")
     options['num'] = yesnoQuestion("Include numerical characters (0-9)?")
@@ -28,28 +29,31 @@ def presentOptions():
     options['similar'] = yesnoQuestion("Include similar characters (I,l,1,0,o,O)?")
     return options
 
+
 def prepareOptions(options):
-    #grab values from dict
+    # grab values from dict
     charToUse = {}
     charToUse['length'] = options['length']
     if options['alpha']:
-        #include similar characters
+        # include similar characters
         alpha = list(string.ascii_lowercase)
         if not options['similar']:
-            similar = set(['i','l','o'])
+            similar = set(['i', 'l', 'o'])
             alphaSet = set(alpha)
             diff = alphaSet.difference(similar)
             alpha = list(diff)
         charToUse['alpha'] = alpha
     if options['caseSensitive']:
+        # include uppercase characters
         caseSensitive = list(string.ascii_uppercase)
         if not options['similar']:
-            similar = set(['I','L','O'])
+            similar = set(['I', 'L', 'O'])
             caseSet = set(caseSensitive)
             diff = caseSet.difference(similar)
             caseSensitive = list(diff)
         charToUse['caseSensitive'] = caseSensitive
     if options['num']:
+        # include numverical characters
         num = list(string.digits)
         if not options['similar']:
             similar = set(['0'])
@@ -58,21 +62,24 @@ def prepareOptions(options):
             num = list(diff)
         charToUse['num'] = num
     if options['symbols']:
+        # include symbols
         symbols = list(string.punctuation)
         symbols.remove('\\')
         charToUse['symbols'] = symbols
     return charToUse
 
+
 def generatePassword(config):
-    length = config['length']
     password = []
+    length = config['length']
     del config['length']
-    #get the first chars for each set of chars. This step is necessary to ensure that at least one char from each charset is used
+    # get the first chars for each set of chars.
+    # This step is necessary to ensure that at least one char from each charset is used
     for c in config:
         password.append(random.choice(config[c]))
-    #fill the remaing chars
+    # fill the remaing chars until desired character amount
     while len(password) < length:
-        #select random item from dictionary
+        # select random item from dictionary
         charset = random.choice(list(config.keys()))
         char = random.choice(config[charset])
         password.append(char)
--- app/main.py
+++ app/main.py
@@ -1,31 +1,33 @@
 import functions as fc
-### dictionary to keep all possible options
-pwdOptions = {}
-
-print("Welcome to the Password generator.\nThis small app has the capability to generate really strong passwords. How would like to proceed, default options, or custom options?")
+# dictionary to keep all possible options
+defaultOptions = {'length': 16, 'alpha': True, 'caseSensitive': True, 'num': True, 'symbols': True, 'similar': False}
+print("Welcome to the Password generator.\nThis small app has the capability to generate really strong passwords. How would like to proceed, default options, or custom options?\nThe default options are:")
+print("Password Lengh:", defaultOptions['length'])
+print("Letters:", defaultOptions['alpha'])
+print("Case sensitive:", defaultOptions['caseSensitive'])
+print("Numbers:", defaultOptions['num'])
+print("Symbols:", defaultOptions['symbols'])
+print("Include similar characters:", defaultOptions['similar'])
 mode = False
-
 
 while mode not in [1,2]:
     mode = int(input("Please choose:\n1 - Default Options (recommended)\n2 - Custom Options\nYour choice: "))
-
 if mode == 1:
-    #generate password with default settings;
-    pwdOptions['length'] = 16
-    pwdOptions['alpha'] = True
-    pwdOptions['caseSensitive'] = True
-    pwdOptions['num'] = True
-    pwdOptions['symbols'] = True
-    pwdOptions['similar'] = False
+    pwdOptions = defaultOptions
 elif mode == 2:
-    #ask user input for the Options
+    # ask user input for the Options
     pwdOptions = fc.presentOptions()
 else:
-    #this should not happen, EVER!!!
+    # this should not happen, EVER!!!
     raise ValueError("Incorrect option passed. This should not happen please investigate.")
 if mode and pwdOptions:
     print("We have all the information needed, so let's start\nThe password will be generated with the following options:")
+    print("Password Lengh:", pwdOptions['length'])
+    print("Letters:", pwdOptions['alpha'])
+    print("Case sensitive:", pwdOptions['caseSensitive'])
+    print("Numbers:", pwdOptions['num'])
+    print("Symbols:", pwdOptions['symbols'])
+    print("Include similar characters:", pwdOptions['similar'])
     pwdConfig = fc.prepareOptions(pwdOptions)
     password = fc.generatePassword(pwdConfig)
     print("Your password is:", ''.join(password))
-    
